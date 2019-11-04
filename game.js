da7b205852ecfa748a27f4c4be9f9d91b0d71171
kovec681 <49417329+kovec681@users.noreply.github.com>
2019-11-04 13:05:49 -0700

    The game script using Hexi as the engine
    Most of this code was copied from an example and some adjustments were made to use our game world map.
    Also added in a basic camera which is not working very well yet.
    Will change how player movement works later on.

--- game.js
+++ game.js
@@ -1,11 +1,244 @@
-//set up gameport, renderer, and stage
-var gameport = document.getElementById("gameport");
-var app = new PIXI.Application({width: 800, height: 500, backgroundColor: 0xffffff});
-gameport.appendChild(app.view);
+/*
+Learn how to import and use data from Tiled Editor.
 
-//animates the stage
-function animate()
-{
-  requestAnimationFrame(animate);
-}
-animate();
+Hexi supports game maps and levels created using the popular Tiled
+Editor level designer:
+
+www.mapeditor.org
+
+To prepare your Tiled Editor game world for use in Hexi, give any significant thing a
+`name` property. Anything with a `name` property in Tiled Editor can
+be accessed in your code by its string name, as you'll see ahead. Tiled Editor layers have a
+`name` property by default, and you can assign custom `name`
+properties to tiles and objects.
+
+Open `maps/timeBomPanic.tmx` file in Tiled Editor and take a careful
+look at how it's been structured. Notice how sprites have been
+organized into layers, and how those layers have been named and
+stacked. Also, notice that the tileset images of the player and bomb
+both have custom `name` properties: "player" and "bomb".
+*/
+
+// Constants
+const WIDTH = 800;
+const HEIGHT = 600;
+const MOVE_SPEED = 4;
+const KEY_LEFT = 37;
+const KEY_RIGHT = 39;
+const KEY_UP = 38;
+const KEY_DOWN = 40;
+
+let thingsToLoad = [
+    "images/world_tileset.png",
+    "images/player.png",
+    "maps/world_map.json"
+  ];
+  
+  //Create a new Hexi instance, and start it.
+  let g = hexi(WIDTH, HEIGHT, setup, thingsToLoad);
+  
+  //Set the background color and scale the canvas
+  //g.backgroundColor = "black";
+  g.scaleToWindow();
+  
+  //Start Hexi
+  g.start();
+  
+  //Game variables
+  let world, camera, player, message, wallMapArray,
+    leftArrow, upArrow, downArrow, rightArrow;
+  
+  //The `setup` function to initialize your application
+  function setup() {
+  
+    //Make the world from the Tiled JSON data and the tileset PNG image
+    world = g.makeTiledWorld(
+      "maps/world_map.json",
+      "images/world_tileset.png"
+    );
+
+    /*
+    Get a reference to the `player` sprite.
+    Use `world.getObject` to do this. `getObject` searches for and
+    returns a sprite in the `world` that has a `name` property that
+    matches the string in the argument.
+    */
+    player = world.getObject("player");
+
+    // Add a world camera to follow the player
+    camera = g.worldCamera(world, world.worldWidth, world.worldHeight);
+    camera.centerOver(player);
+
+    /*
+    Each Tiled Editor layer has a `name` that can be accessed in your
+    game code using
+    `world.getObject` Tiled Editor's `tilelayers` have a `data` property
+    that is an array containing all the grid index numbers (`gid`) of
+    the tiles in that array. In this example we want to access all the
+    wall sprites. In Tiled Editor, all the wall sprites were added to
+    a tile layer called `wallLayer`. We can access the `wallLayer`'s
+    `data` array of sprites like this:
+    */
+  
+    wallMapArray = world.getObject("wallLayer").data;
+  
+    /*
+    We also need a reference to the bomb layer. All Tiled Editor layers are
+    created as `groups` by Hexi's `makeTiledWorld` method. That means they
+    all have a `children` array that lets' you access all the sprites on
+    that layer, if you even need to do that.
+    */
+  
+    //bombLayer = world.getObject("bombLayer");
+  
+    //Get a reference to the level's bomb layer array. This is the
+    //bomb layer's `data` array
+  
+    //bombMapArray = bombLayer.data;
+  
+    /*
+    You can use `world.getObjects` (with an "s") to get an array of all
+    the things in the world that have the same `name` properties. There
+    are 5 bombs in the world, all which have share the same `name`
+    property: "bomb". Here's how you can access to all of them in an
+    array:
+    */
+  
+    //bombSprites = world.getObjects("bomb");
+  
+    //`bombSprites` is now an array that contains all the bomb sprites
+    //in the world
+  
+    //Give the `player` a `direction` property
+    player.direction = "";
+  
+    //Configure Hexi's built in arrow keys to assign the player a direction
+    //Create some keyboard objects
+    leftArrow = g.keyboard(KEY_LEFT);
+    upArrow = g.keyboard(KEY_UP);
+    rightArrow = g.keyboard(KEY_RIGHT);
+    downArrow = g.keyboard(KEY_DOWN);
+  
+    //Program the keyboard objects
+    leftArrow.press = () => player.direction = "left";
+    upArrow.press = () => player.direction = "up";
+    rightArrow.press = () => player.direction = "right";
+    downArrow.press = () => player.direction = "down";
+  
+    //Change the game state to `play`
+    g.state = play;
+  }
+  
+  //The `play` function contains all the game logic and runs in a loop
+  function play() {
+  
+    //Change the player's direction only if it's at an intersection
+    //(This keeps it aligned to the grid cells. You don't have to do
+    //this but it's a nice effect that you might want to use in your
+    //own games at some point.)
+  
+    if (Math.floor(player.x) % world.tilewidth === 0 && Math.floor(player.y) % world.tileheight === 0) {
+      switch (player.direction) {
+        case "up":
+          player.vy = -MOVE_SPEED;
+          player.vx = 0;
+          break;
+        case "down":
+          player.vy = MOVE_SPEED;
+          player.vx = 0;
+          break;
+        case "left":
+          player.vx = -MOVE_SPEED;
+          player.vy = 0;
+          break;
+        case "right":
+          player.vx = MOVE_SPEED;
+          player.vy = 0;
+          break;
+        case "none":
+          player.vx = 0;
+          player.vy = 0;
+          break;
+      }
+      camera.follow(player);
+    }
+  
+    //Move the player
+    g.move(player);
+  
+    //Keep the player contained inside the canvas
+    //g.contain(player, g.stage);
+  
+    /*
+    Prevent the player from walking through walls using the
+    versatile `hitTestTile` method. `hitTestTile` checks for a
+    collision between a sprite and a tile in any map array that you
+    specify. It returns a `collision` object.
+    `collision.hit` is a Boolean that tells you if a sprite is colliding
+    with the tile that you're checking. `collision.index` tells you the
+    map array's index number of the colliding sprite. You can check for
+    a collision with the tile against "every" corner point on the
+    sprite, "some" corner points, or the sprite's "center" point. (Each
+    of these three options has a different and useful effect, so experiment with
+    them.)
+  
+    `hitTestTile` arguments:
+    sprite, array, collisionTileGridIdNumber, worldObject, spritesPointsToCheck
+  
+    The `world` object (the 4th argument) has to have these properties:
+    `tileheight`, `tilewidth`, `widthInTiles`.
+  
+    `hitTestTile` will work for any map array, not just those made with
+    Tiled Editor. So you can use it with your own game maps in the same way.
+  
+    */
+  
+    let playerVsFloor = g.hitTestTile(player, wallMapArray, 0, world, "every");
+  
+    //If every corner point on the player isn't touching a floor tile (array gridIDNumber: 0) then
+    //prevent the player from moving
+    //
+    if (!playerVsFloor.hit) {
+  
+      //To prevent the player from moving, subtract its velocity from its position
+      player.x -= player.vx;
+      player.y -= player.vy;
+      player.vx = 0;
+      player.vy = 0;
+    }
+/*  
+    //Let the player pick up bombs
+    let playerVsBomb = g.hitTestTile(player, bombMapArray, 5, world, "every");
+  
+    //Find out if the player's position in the bomb array matches a bomb gid number
+    if (playerVsBomb.hit) {
+  
+      //If it does, filter through the bomb sprites and find the one
+      //that matches the player's position
+      bombSprites = bombSprites.filter(function(bomb) {
+  
+        //Does the bomb sprite have the same index number as the player?
+        if (bomb.index === playerVsBomb.index) {
+  
+          //If it does, remove the bomb from the
+          //`bombMapArray` by setting its gid to `0`
+          bombMapArray[bomb.index] = 0;
+  
+          //Remove the bomb sprite from its container group
+          g.remove(bomb);
+  
+          //Alternatively, remove the bomb with `removeChild` on
+          //the `bombLayer` group
+          //bombLayer.removeChild(bomb);
+          //Filter the bomb out of the `bombSprites` array
+          return false;
+        } else {
+  
+          //Keep the bomb in the `bombSprites` array if it doesn't match
+          return true;
+        }
+      });
+    }
+*/
+  }
+  
