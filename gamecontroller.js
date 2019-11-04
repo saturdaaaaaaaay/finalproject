ee8ba6fbda3259d2368a4b998063e55e6dfe6036
saturdaaaaaaaay <ecc226@nau.edu>
2019-11-04 14:56:12 -0700

    Created gamecontroller and added state-machine.js

---
+++ gamecontroller.js
@@ -0,0 +1,35 @@
+// Alias Declarations
+let AnimatedSprite = PIXI.AnimatedSprite;
+let Container = PIXI.Container;
+let Loader = PIXI.Loader;
+let Sprite = PIXI.Sprite;
+let sound = PIXI.sound;
+let Texture = PIXI.Texture;
+
+class Character
+{
+
+}
+
+class PC extends Character
+{
+
+}
+
+class NPC extends Character
+{
+
+}
+
+class Item
+{
+  constructor(itemID)
+  {
+    this.itemID = 0;
+  }
+}
+
+class Dialogue
+{
+
+}
