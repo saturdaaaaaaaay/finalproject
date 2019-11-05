bf7d05f7759953305a0167c306382a231025f9f0
kovec681 <49417329+kovec681@users.noreply.github.com>
2019-11-05 11:16:27 -0700

    Uploaded source files
    TMX files are used in Tiled to define the maps.
    TSX files are used in Tiled as the tilesets that contain the resources.

---
+++ source/tileset.tsx
@@ -0,0 +1,72 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="tileset" tilewidth="100" tileheight="100" tilecount="10" columns="10">
+ <image source="../images/tileset.png" width="1000" height="100"/>
+ <tile id="0">
+  <properties>
+   <property name="name" value="bridge"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="100" height="100"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="name" value="building"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="100" height="100"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="name" value="door"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="name" value="floor"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="name" value="forest"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="100" height="100"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="name" value="grass"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="name" value="player"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="100" height="100"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="name" value="river"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="100" height="100"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="name" value="square"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="name" value="wall"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="100" height="100"/>
+  </objectgroup>
+ </tile>
+</tileset>
