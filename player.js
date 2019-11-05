48ce29b694df6dd752466658f78dc437d56bb548
Rebecca Leggett <roboticsgirl60@gmail.com>
2019-11-05 10:37:52 -0700

    added code to check for items

--- player.js
+++ player.js
@@ -4,7 +4,10 @@
 // array of quest states?
 
 
-var npcs = [];
+//var npcs = [];
+
+//var current_items = [];
+
 
 function checkForNPC(player_character)
 {
@@ -23,8 +26,40 @@ function checkForNPC(player_character)
 	}
 	if (hit === true)
 	{
+		console.log(current_npc.name);
 		current_npc.interact();
 	}
 }
 
+function checkForItem(player_character)
+{
+	//items
+	let i;
+	let hit = false;
+	let current_item;
+	for (i = 0; items.length; i++)
+	{
+		hit = false;
+		current_item = items[i];
+		hit = g.hit(player_character, current_item);
+		if (hit === true)
+		{
+			break;
+		}
+	}
+	if (hit === true)
+	{
+		//items.interact();
+		console.log(current_item.name);
+		
+		//current_items.push(current_item);
+	}
+	
+}
 
+/*
+function checkForQuestItem()
+{
+	
+}
+*/
