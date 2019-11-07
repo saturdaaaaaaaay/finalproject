fc269e5e7dfd9f161cef8aa836852dd25e4918cc
kovec681 <49417329+kovec681@users.noreply.github.com>
2019-11-07 10:45:34 -0700

    added new constants

--- global.js
+++ global.js
@@ -6,10 +6,19 @@
 const WIDTH = 800;
 const HEIGHT = 600;
 const MOVE_SPEED = 4;
+
 const KEY_LEFT = 37;
 const KEY_RIGHT = 39;
 const KEY_UP = 38;
 const KEY_DOWN = 40;
+
+const KEY_LEFT_ALT = "a";
+const KEY_RIGHT_ALT = "d";
+const KEY_UP_ALT = "w";
+const KEY_DOWN_ALT = "s";
+
+const ACTION_KEY = "32";
+const QUEST_KEY = "q";
 
 //Game declarations
 let g, thingsToLoad, world, camera, player, quest_NPC, reg_NPC, item, message, wallMapArray,
