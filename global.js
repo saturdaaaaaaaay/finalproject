51f413f68c393acbad4d5bbc2525b4c277768cc5
kovec681 <49417329+kovec681@users.noreply.github.com>
2019-11-11 21:37:48 -0700

    Added more global declarations

--- global.js
+++ global.js
@@ -12,14 +12,30 @@ const KEY_RIGHT = 39;
 const KEY_UP = 38;
 const KEY_DOWN = 40;
 
-const KEY_LEFT_ALT = "a";
-const KEY_RIGHT_ALT = "d";
-const KEY_UP_ALT = "w";
-const KEY_DOWN_ALT = "s";
+const KEY_LEFT_ALT = 65;
+const KEY_RIGHT_ALT = 68
+const KEY_UP_ALT = 87;
+const KEY_DOWN_ALT = 83;
 
-const ACTION_KEY = "32";
-const QUEST_KEY = "q";
+const ACTION_KEY = 32;
+const QUEST_KEY = 81;
 
 //Game declarations
-let g, thingsToLoad, world, camera, player, quest_NPC, reg_NPC, item, message, wallMapArray,
-    leftArrow, upArrow, downArrow, rightArrow, npcs, items;
+let g,
+    thingsToLoad,
+    world,
+    building,
+    camera,
+    player,
+    quest_NPC,
+    reg_NPC,
+    item,
+    message,
+    wallMapArray,
+    leftArrow,
+    upArrow,
+    downArrow,
+    rightArrow,
+    interact,
+    npcs,
+    items;
