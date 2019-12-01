c0317c375873137be1119a7f1ca2e358a2f1b496
kovec681 <49417329+kovec681@users.noreply.github.com>
2019-11-30 18:50:49 -0700

    Latest source files for maps

---
+++ source/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="tileset" tilewidth="64" tileheight="64" tilecount="40" columns="4">
+ <image source="project4/images/tileset.png" width="256" height="640"/>
+</tileset>
